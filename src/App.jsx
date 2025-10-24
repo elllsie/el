c2e25d7e4cefd20d8bcd1e4adc77c4277259faf6
elsie <870864243@qq.com>
2025-10-24 19:04:38 +0800

    Update source code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Gallery from "./pages/Gallery";
 import About from "./pages/About";
