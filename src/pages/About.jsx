df1d7beb3bfa5a658191e09190bc40ad3fd3551b
elsie <870864243@qq.com>
2025-10-26 15:32:54 +0800

    change classname to class

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,16 @@
 export default function About() {
     return (
-      <div clasçsName="p-10 max-w-3xl mx-auto text-center leading-relaxed">
-        <h2 className="text-4xl font-light mb-8">About Me</h2>
-        <p className="mb-6 text-gray-700">
+      <div class="p-10 max-w-3xl mx-auto text-center leading-relaxed">
+        <h2 class="text-4xl font-light mb-8">About Me</h2>
+        <p class="mb-6 text-gray-700">
           I’m a software testing engineer with several years of experience in QA
           and automation. 
         </p>
-        <p className="mb-6 text-gray-700">
+        <p class="mb-6 text-gray-700">
           During a recent gap, I dedicated time to painting. This creative pursuit
           helped me refine my patience, detail orientation, and storytelling.
         </p>
-        <p className="text-gray-700">
+        <p class="text-gray-700">
           I built this po【prtfolio to showcase both my artwork and my continuous
           learning spirit.
         </p>
